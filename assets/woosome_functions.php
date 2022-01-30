a6e06ba967c79b6ad04d8613ec1760d7e131647f
webbouwer <support@webdesigndenhaag.net>
2022-01-30 13:02:19 +0100

    Update ajax to photoswipe

--- assets/woosome_functions.php
+++ assets/woosome_functions.php
@@ -33,8 +33,8 @@ function gallery_scripts() {
 function pswp_box_html() {
 echo '<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true"><div class="pswp__bg"></div><div class="pswp__scroll-wrap"><div class="pswp__container"><div class="pswp__item"></div><div class="pswp__item"></div><div class="pswp__item"></div></div><div class="pswp__ui pswp__ui--hidden"><div class="pswp__top-bar"><div class="pswp__counter"></div><button class="pswp__button pswp__button--close" aria-label="Close (Esc)"></button><button class="pswp__button pswp__button--share" aria-label="Share"></button><button class="pswp__button pswp__button--fs" aria-label="Toggle fullscreen"></button><button class="pswp__button pswp__button--zoom" aria-label="Zoom in/out"></button><div class="pswp__preloader"><div class="pswp__preloader__icn"><div class="pswp__preloader__cut"><div class="pswp__preloader__donut"></div></div></div></div></div><div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap"><div class="pswp__share-tooltip"></div></div><button class="pswp__button pswp__button--arrow--left" aria-label="Previous (arrow left)"></button><button class="pswp__button pswp__button--arrow--right" aria-label="Next (arrow right)"></button><div class="pswp__caption"><div class="pswp__caption__center"></div></div></div></div></div>';
 }
-add_action( 'wp_footer', 'pswp_box_html' );
-add_action( 'wp_footer', 'woocommerce_photoswipe' );
+add_action( 'woocommerce_after_shop_loop', 'pswp_box_html' );
+add_action( 'woocommerce_after_shop_loop', 'woocommerce_photoswipe' ); // woocommerce_after_shop_loop
 
 
 // add js libs
